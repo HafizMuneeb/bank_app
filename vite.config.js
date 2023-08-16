c2dfb5a3b2df33c3668dfdb43e2c9c5e704c6a1c
Muneeb <muneebafzal381a@gmail.com>
2023-08-16 12:08:56 +0500

    .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-MODERN-BANK-APP/'
+  base: '/vite-deploy-bank_app/'
 })
